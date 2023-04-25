f0bcd2232d1d6a4ae19377dd893823617868908a
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-25 12:20:56 +0530

    ✨ Adding buttons for routing

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './home.css'
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <Link to="/income">
+        <Button variant="contained" color="secondary">Income</Button>
+      </Link>
+      <Link to="/price">
+        <Button variant="contained" color="error">Price</Button>
+      </Link>
+      <Link to="/brand">
+        <Button variant="contained" color="info">Brand</Button>
+      </Link>
+      <Link to="/name">
+        <Button variant="contained" color="warning">Name</Button>
+      </Link>
+      <Link to="/cities">
+        <Button variant="contained" color="success">Cities</Button>
+      </Link>
+    </div>
   )
 }
 
